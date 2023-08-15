1cc4e60e5d2f0e12191d03221e8b5dbb2a3afc9d
JeonTOM <jjh6593@naver.com>
2023-08-15 13:44:32 +0900

    1.2.1 fix img

--- src/App.js
+++ src/App.js
@@ -18,16 +18,20 @@ function App() {
   return (
     <div className="App">
       <h1 id='title'>약관 상세 분석 시스템 {selectedValue}</h1>
+      <div style={{ display: 'grid', gridTemplateColumns: '1fr 3fr 1fr' }}>
+      <div className='remain'>
       {selectedImage && (
         <img
           src={URL.createObjectURL(selectedImage)}
           alt="Uploaded"
           className="img-uploaded"
         />
-      )}
+      )}</div>
       <div className="container">
         <InputComponent handleInputChange={handleInputChange} setImage={setImage} />
         <OutputComponent userInput={userInput} setSelectedValue={setSelectedValue} />
+      </div>
+      <div className='remain'></div>
       </div>
 
     </div>
